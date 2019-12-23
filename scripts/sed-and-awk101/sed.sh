c4b2332f6f8ab381a308f04e106e0c67fdf400ec
yangzhao <15339190665@163.com>
2019-12-24 01:09:28 +0800

    update

--- scripts/sed-and-awk101/sed.sh
+++ scripts/sed-and-awk101/sed.sh
@@ -45,20 +45,6 @@ sed -n '/^103/ p' employee.txt
 # 匹配r结尾的line
 sed -n '/r$/ p' employee.txt
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # 过滤出log:后面又数据的line（含义是可以有任何东西但是必须有一个）
 sed -n '/log: *./ p' log.txt
 
@@ -82,3 +68,84 @@ sed '/s/Manager .*//' employee.txt
 
 # 去掉html文件中的tag标签
 sed -e 's/<[^<]*>//g'  test.html
+
+# 多个命令一起执行
+sed -n '{
+	/^root/ p
+	/^nobody/ p
+	/^mail/ p
+}' /etc/passwd
+
+# 直接修改结果到文件
+sed -i 's/john/johnny' employee.txt
+
+# 修改结果到文件，同时会保存一个备份文件
+sed -ibak 's/john/johnny' employee.txt
+
+# 追加line在特定的位置 a insert after
+sed '[address] a the-line-to-append' input-file
+
+# 添加203,jack Engineer 在第二行后面
+sed '2 a 203,jack Engineer' employee.txt
+
+# 添加两行
+sed '/Jason/a\
+203,Jack Johnson,Engineer\
+204,Mark Smith,Sales Engineer' employee.txt
+
+# insert before
+sed '2 i 203,Jack Johnson,Engineer' employee.txt 
+
+# 替换第二行的数据
+sed '2 c 203,jack johnson,Engineer' employee.txt
+
+# 输出行号
+sed = employee.txt
+
+# 匹配jane 的line加上行号
+sed '/jane/ =' employee.txt
+
+# 替换字符串：y,将文件中的a全部替换成b
+sed 'y/a/b' employee.txt
+
+# 匹配多个文件
+sed -n '/root/ p' /etc/passwd /etc/group
+
+# r 读取，下面语句会在最后一行读取log.txt 并输出出来
+sed '$ r log.txt' employee.txt
+
+# -c 等于--copy 
+sed -ibak -c 's/john/johnny/' employee.txti
+
+# n 为模式空间
+sed -n n employee.txt
+
+# x 交换模式空间（pattern space） 和hold space（保持空间）
+# {x;n} 交换模式空间到保持空间，n读取了下一行到模式空间中，所以name在保持空间，title在模式空间中
+# /Manager/{x;p} 匹配Manager，交换模式空间和保持空间，输出
+sed -n -e 'x;n' -e '/Manager/{x;p}' empnametitle.txt
+
+# /Manager/!h 不等于/Manager的复制到保持空间中
+# /Manager/{x;p} 匹配Manager的交换模式空间和保持空间，然后输出x
+sed -n -e '/Manager/!h' -e '/Manager/{x;p}' empnametitle.txt
+
+# /Manager/{H;x;p} 先追加模式空间到保持空间，在转换，再输出H
+sed -n -e '/Manager/!h' -e '/Manager/{H;x;p}' empnametitle.txt
+
+# /Manager/{H;x;s/\n/:/;p} s将定界符/n 替换成;
+sed -n -e '/Manager/!h' -e '/Manager/{H;x;s/\n/:/;p}' empnametitle.txt
+
+# g：将保持空间中的数据复制到模式空间中，h相当于存到保持空间，g相当于从保存空间中获取
+sed -n -e '/Manager/!h' -e '/Manager/{g;p}' empnametitle.txt
+
+# G: 追加保持空间的数据到模式空间中
+sed -n -e '/Manager/!h' -e '/Manager/{x;G;s/\n/:/;p}' empnametitle.txt
+
+# N: 追加一行到模式空间中（模式空间中本来有employee name，下一行再追加employee title，）
+sed -e '{N;s/\n/:/}' empnametitle.txt
+
+# =对每一行加序号，N追加第二行到模式空间中，再将\n的定位符转换成 空格
+sed -e '=' employee.txt | sed -e '{N;s/\n/ /}'
+
+# P：print 1st line in multiline:多行中输入第一行
+ sed -n -e 'N' -e '/Manager/P' empnametitle.txt
